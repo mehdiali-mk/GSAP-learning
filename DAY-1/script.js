0ae445079b277f922943733bc595c2fa5d7212b9
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2024-08-27 23:15:38 +0530

    DAY 1 - Basic div current animations.

---
+++ DAY-1/script.js
@@ -0,0 +1,52 @@
+gsap.to("#box1", {
+  x: 480,
+  duration: 2,
+  delay: 1,
+  repeat: 1,
+  yoyo: true,
+});
+
+gsap.to("#box2", {
+  x: -480,
+  duration: 2,
+  delay: 1,
+  repeat: 1,
+  yoyo: true,
+});
+
+gsap.to("#box3", {
+  x: 480,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  repeat: 1,
+  yoyo: true,
+});
+
+gsap.to("#box4", {
+  x: -480,
+  duration: 2,
+  delay: 1,
+  scale: 1.5,
+  repeat: 1,
+  yoyo: true,
+});
+
+gsap.to("#box5", {
+  x: 480,
+  duration: 2,
+  delay: 1,
+  scale: 0.5,
+  repeat: 1,
+  yoyo: true,
+});
+
+gsap.to("#box6", {
+  x: 480,
+  duration: 2,
+  delay: 1,
+  borderRadius: "50%",
+  backgroundColor: "#5c5470",
+  repeat: 1,
+  yoyo: true,
+});
